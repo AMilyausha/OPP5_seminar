eb5e15948f6fc1aaacf5bee83abeddb796ec3eea
Amirova Milyausha <77613038+AMilyausha@users.noreply.github.com>
2023-01-29 19:07:37 +0300

    Update Log.java

--- Log.java
+++ Log.java
@@ -5,7 +5,7 @@ public class Log {
     // String FILE_NAME_EXPORT = "log.csv";
     // String FILE_PATH_FULL = FILE_PATH+FILE_NAME_EXPORT;
     public void LogText(String log){
-        Path path = Paths.get("C:\\Users\\user\\Desktop\\OPP_5_SEM\\");
+        Path path = Paths.get("C:\\Users\\user\\Desktop\\OPP_5_SEM\\log.csv");
         try {
             Files.write(path, log, StandardOpenOption.APPEND);
         } catch (IOException e) {
