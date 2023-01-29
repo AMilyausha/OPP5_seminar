a1bc5128cd0d7aadec93318e15072717062fd0d1
Amirova Milyausha <mila17.86@mail.ru>
2023-01-29 18:47:48 +0300

    Пятый семинар

---
+++ Log.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class Log {
+ 
+    // String FILE_PATH = "./src/main/java/org/phonebook/";
+    // String FILE_NAME_EXPORT = "log.csv";
+    // String FILE_PATH_FULL = FILE_PATH+FILE_NAME_EXPORT;
+    public void LogText(String log){
+        Path path = Paths.get("C:\\Users\\user\\Desktop\\OPP_5_SEM\\");
+        try {
+            Files.write(path, log, StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    }
+  
+   
+
